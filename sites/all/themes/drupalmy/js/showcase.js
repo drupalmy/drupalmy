d5f42dd06b0993fb660468b3d6a2d91d1e00d4ca
Edmund Kwok <edmund.kwok@insyghtful.com>
2012-03-02 01:08:59 +0800

    Add Showcase feature

---
+++ sites/all/themes/drupalmy/js/showcase.js
@@ -0,0 +1,14 @@
+(function ($){
+  Drupal.behaviors.showcaseHover = {
+    attach: function(context, settings) {
+      $('.views-field-body').css('opacity', 0.8);
+      $('.view-showcase li', context).once('li', function() {
+        $(this).hover(function() {
+          $('.views-field-body', this).stop(false, true).slideDown('fast');
+        }, function() {
+          $('.views-field-body', this).stop(false, true).slideUp('fast');
+        });
+      })
+    }
+  }
+})(jQuery);
