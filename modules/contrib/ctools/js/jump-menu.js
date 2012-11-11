ba249500d2685622a55098073d04384545e78c59
Jonathan Loh <jon@gix.my>
2012-11-11 22:44:55 +0800

    Revert "Revert "Revert "Updated contributed modules."""
    This reverts commit f30852a53531aff74aed6c3b85e97c10a96c05ac.

--- modules/contrib/ctools/js/jump-menu.js
+++ modules/contrib/ctools/js/jump-menu.js
@@ -1,13 +1,13 @@
 
 (function($) {
-  Drupal.behaviors.CToolsJumpMenu = {
+  Drupal.behaviors.CToolsJumpMenu = { 
     attach: function(context) {
-      $('.ctools-jump-menu-hide')
-        .once('ctools-jump-menu')
+      $('.ctools-jump-menu-hide:not(.ctools-jump-menu-processed)')
+        .addClass('ctools-jump-menu-processed')
         .hide();
 
-      $('.ctools-jump-menu-change')
-        .once('ctools-jump-menu')
+      $('.ctools-jump-menu-change:not(.ctools-jump-menu-processed)')
+        .addClass('ctools-jump-menu-processed')
         .change(function() {
           var loc = $(this).val();
           var urlArray = loc.split('::');
@@ -20,8 +20,8 @@
           return false;
         });
 
-      $('.ctools-jump-menu-button')
-        .once('ctools-jump-menu')
+      $('.ctools-jump-menu-button:not(.ctools-jump-menu-processed)')
+        .addClass('ctools-jump-menu-processed')
         .click(function() {
           // Instead of submitting the form, just perform the redirect.
 
